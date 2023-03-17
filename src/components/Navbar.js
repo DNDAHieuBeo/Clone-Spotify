9bc797b82ea07956ada2e43940761b0388b3e4a5
DNDAHieuBeo <90563964+DNDAHieuBeo@users.noreply.github.com>
2023-03-17 22:13:12 +0700

    build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return(
         <div className="h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
             <FaSpotify className="inline mr-4"/>
-            Spotify
+            Anh Lân đi bộ
         </div>
     )
 }
